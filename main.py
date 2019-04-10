528ad85248ccbc36faf8c8ef032934215923e8b9
pavlo <pavlo@localhost.localdomain>
2019-04-10 14:14:00 +0300

    --

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import vk_api
 import os
 import random
+import time
 
 def ping(target:str):
-    if os.system("ping -q -c 10 " + target +">>/dev/null") == 0:
+    if os.system("ping -q -c 10 " + target +" > /dev/null") == 0:
         print(target.replace('\n', "") + " is Up")
     else:
         print(target + " is down")
-        bot = vk_api.VkApi(token="ff37122dadc19fe93680a55ec5a8dd09c7babcd7864b4657aa5fe47cc8eec7da3fbf48fffca6e388d534f")
+        bot = vk_api.VkApi(token=open("token", "r").readline())
         bot._auth_token()
         bot.method("messages.send",{"peer_id": 160500068, "random_id" : random.randint(1, 1000),"message": (target.replace('\n', "") + " is down")})
 
 def main():
-    for line in open("config", "r"):
-        if line != "":
-            ping(line)
-
-    #ping("misroka.ru")
-
+    while True:
+        for line in open("config", "r"):
+            if line != "":
+                ping(line)
+        time.sleep(3600)
 main()
