4a64e84233caa5575b0cf4daa352edf7be00cbad
HoolyPanda <pavlov.ss10@gmail.com>
2019-06-02 18:33:32 +0300

    check if bot is alive

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 def ping(target:str):
     a = os.system("nmap -sn -PS " + "192.168.0.4 | grep \"Host is up\"")
     if os.system("nmap -sn -PS " + target.replace('\n',"") + " | grep \"Host is up\"") == 0:
-        print(target.replace('\n', "") + " is Up")
+        print("======================================  " + target.replace('\n', "") + " is Up")
     else:
         print(target + " is down")
         bot = vk_api.VkApi(token=open("token", "r").readline())
@@ -14,9 +14,15 @@ def ping(target:str):
         bot.method("messages.send",{"peer_id": 160500068, "random_id" : random.randint(1, 1000),"message": (target.replace('\n', "") + " is down")})
 
 def main():
+    i = 0
     while True:
         for line in open("config", "r"):
             if line != "":
                 ping(line)
+        i += 1
+        if i == 12:
+            bot = vk_api.VkApi(token=open("token", "r").readline())
+            bot._auth_token()
+            bot.method("messages.send",{"peer_id": 160500068, "random_id" : random.randint(1, 1000),"message": ("Бот все еще в сети")})
         time.sleep(3600)
 main()
