db18742a8f149670329d2ae9c9efe86fc4cd15c4
HoolyPanda <pavlov.ss10@gmail.com>
2019-07-20 02:10:04 +0300

    bugfix

--- main.py
+++ main.py
@@ -24,5 +24,6 @@ def main():
             bot = vk_api.VkApi(token=open("token", "r").readline())
             bot._auth_token()
             bot.method("messages.send",{"peer_id": 160500068, "random_id" : random.randint(1, 1000),"message": ("Бот все еще в сети")})
+            i = 0
         time.sleep(3600)
 main()
